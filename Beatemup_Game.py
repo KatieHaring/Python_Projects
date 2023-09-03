9de0acaf2ca09acaa2ff6507c2c58a463da25b0f
KatieHaring <kmhst25@mail.rmu.edu>
2023-09-03 13:23:30 -0400

    Added taunts, replaced level with damage, replaced potions with sandwiches, randomized enemy special attacks, cleaned up text, added comments

--- Beatemup_Game.py
+++ Beatemup_Game.py
@@ -16,174 +16,188 @@ print(beatemup)
 
 
 
-
 # Create Enemy class
 class Enemy:
-  def __init__(self, name, level, damage, special_act, health=0):
+  def __init__(self, name, damage, special_act, health=0):
     self.name = name
-    self.level = level
-    self.health = level * 5
     self.damage = damage
+    self.health = damage * 5
     self.special_act = special_act
 
   # Describe the enemy
   def __repr__(self):
-    description = "This level {level} enemy is called {name}. They have {health} points of health and deal {damage} damage. Their special action is {special_act}.".format(level = self.level, name = self.name, health = self.health, damage = self.damage, special_act = self.special_act)
+    description = "This enemy is called {name}. They have {health} points of health and deal {damage} damage. Their special action is {special_act}.".format(name = self.name, health = self.health, damage = self.damage, special_act = self.special_act)
     return description
 
   # Slap the player
   def slap(self, player):
-    print("{name1} slaps {name2}. It doesn't hurt her, but it's super rude.".format(name1=self.name, name2=player.name))
+    print("{name1} slaps {name2}.".format(name1=self.name, name2=player.name))
+    print("It doesn't hurt her, but it's super rude.")
 
   # Punch the player
-  def punch(self, player):
+  def punch(self, player): 
+    # Punching inflicts base damage
     player.health -= self.damage
+    print("{name1} punches {name2}.".format(name1=self.name, name2=player.name))    
     if player.health <= 0 :
-      print("{name1} punches {name2}.".format(name1=self.name, name2=player.name))
       print("{name2} has been knocked out!".format(name2=player.name))
       print("You lose!")
-    else:
-      print("{name1} punches {name2}. {name2} takes {damage} damage!".format(name1=self.name, name2=player.name, damage=int(self.damage)))
+    else:    
+      print("{name2} takes {damage} damage!".format(name2=player.name, damage=int(self.damage)))
 
   # Kick the player
   def kick(self, player):
+    # Kicking inflicts base damage * 3/2
     player.health -= self.damage*3/2
+    print("{name1} kicks {name2}.".format(name1=self.name, name2=player.name))    
     if player.health <= 0 :
-      print("{name1} kicks {name2}.".format(name1=self.name, name2=player.name))
       print("{name2} has been knocked out!".format(name2=player.name))
       print("You lose!")
     else:
-      print("{name1} kicks {name2}. {name2} takes {damage} damage!".format(name1=self.name, name2=player.name, damage=int(round(self.damage,0))))
+      print("{name2} takes {damage} damage!".format(name2=player.name, damage=int(round(self.damage,0))))
         
   # Use a special attack on the player 
   def use_special(self, player):
-    if self.special_act == "Potion":
+    if self.special_act == "Sandwich":
+      # Sandwiches restore 5 health 
       self.health += 5
-      print("{name} used a potion and regained 5 health!".format(name=self.name))       
+      print("{name} eats a sandwich and regains 5 health!".format(name=self.name))       
 
     elif self.special_act == "Flying Kick":
+      # Flying kicks do 2 times base damage
       player.health -= self.damage*2
+      print("{name1} uses a flying kick on {name2}.".format(name1=self.name, name2=player.name)) 
       if player.health <= 0:
-        print("{name1} used a flying kick on {name2}.".format(name1=self.name, name2=player.name))       
         print("{name2} has been knocked out!".format(name2=player.name))
         print("You lose!")
-      else:
-        print("{name1} used a flying kick on {name2}. {name2} takes {damage} damage!".format(name1=self.name, name2=player.name, damage=int(self.damage*2)))       
+      else:            
+        print("{name2} takes {damage} damage!".format(name2=player.name, damage=int(self.damage*2)))       
 
     elif self.special_act == "Multiple Jabs":
+      # Multiple jabs do 3 times base damage 
       player.health -= self.damage*3
+      print("{name1} uses multiple jabs on {name2}.".format(name1=self.name, name2=player.name))      
       if player.health <= 0:
-        print("{name1} used multiple jabs on {name2}.".format(name1=self.name, name2=player.name))       
         print("{name2} has been knocked out!".format(name2=player.name))
         print("You lose!")
       else:
-        print("{name1} used multiple jabs on {name2}. {name2} takes {damage} damage!".format(name1=self.name, name2=player.name, damage=int(self.damage*3)))       
+        print("{name2} takes {damage} damage!".format(name2=player.name, damage=int(self.damage*3)))       
 
     elif self.special_act == "Drain":
+      # Drain takes base damage health from the player
       begin_health = player.health
       player.health -= min(begin_health, self.damage)
       self.health += min(begin_health, self.damage)
+      print("{name1} uses drain to take {damage} health from {name2}!".format(name1=self.name, name2=player.name, damage=int(round(min(begin_health, self.damage),0))))               
       if player.health <= 0:
-        print("{name1} took {damage} health from {name2}.".format(name1=self.name, name2=player.name, damage=int(round(min(begin_health, self.damage),0))))         
         print("{name2} has been knocked out!".format(name2=player.name))
-        print("You lose!")
-      else:
-        print("{name1} used drain. {name1} took {damage} health from {name2}!".format(name1=self.name, name2=player.name, damage=int(self.damage)))         
-        
+        print("You lose!")        
 
 
 
 # Create Player class
 class Player:
-  def __init__(self, name, level, damage, special_act, health=0):
+  def __init__(self, name, damage, special_act, health=0):
     self.name = name
-    self.level = level
-    self.health = level * 5
-    self.damage = damage   
+    self.damage = damage       
+    self.health = damage * 5
     self.special_act = special_act
 
   # Describe the Player 
   def __repr__(self):
-    description = "This level {level} player is called {name}. They have {health} points of health and deal {damage} damage. Their special action is {special_act}.".format(level = self.level, name = self.name, health = self.health, damage = self.damage, special_act = self.special_act)
+    description = "This player is called {name}. They have {health} points of health and deal {damage} damage. Their special action is {special_act}.".format(name = self.name, health = self.health, damage = self.damage, special_act = self.special_act)
     return description
 
   # Punch the enemy
   def punch(self, enemy):
+    # Punching inflicts base damage
     enemy.health -= self.damage
+    print("{name1} punches {name2}.".format(name1=self.name, name2=enemy.name))    
     if enemy.health <= 0 :
-      self.level += 1
+      self.damage += 1
       print("{name} has been knocked out!".format(name=enemy.name))
     else:
-      print("{name} took {damage} damage!".format(name=enemy.name, damage=int(self.damage)))   
+      print("{name2} takes {damage} damage!".format(name2=enemy.name, damage=int(self.damage)))
 
   # Kick the enemy
   def kick(self, enemy):
+    # Kicking inflicts base damage * 3/2
     enemy.health -= self.damage*3/2 
+    print("{name1} kicks {name2}.".format(name1=self.name, name2=enemy.name))     
     if enemy.health <= 0 :
-      self.level +=1
+      self.damage +=1
       print("{name} has been knocked out!".format(name=enemy.name))
     else:
-      print("{name} took {damage} damage!".format(name=enemy.name, damage=int(round(self.damage*3/2,0)))) 
-
+      print("{name2} takes {damage} damage!".format(name2=enemy.name, damage=int(round(self.damage*3/2,0)))) 
+ 
   # Taunt the enemy
   def taunt(self, enemy):
-    print("{name1} calls {name2} a dirty landlubber. They don't know what to say to that...".format(name1=self.name, name2=enemy.name))
+    # Create a list of possible taunts
+    taunts = ["a dirty landlubber", "stinkier than Steubenville", "a pathetic snollygoster", "all hat and no cattle", 
+              "less useful than belt dressing", "lacking in creativity", "a worthless gadabout"]
+    # Pick a random taunt
+    print("{name1} calls {name2} ".format(name1=self.name, name2=enemy.name) + random.choice(taunts) + ".")
+    print("They don't know what to say to that...")    
 
-  # Use a special attack
+  # Use a special attack on the enemy
   def use_special(self, enemy):
-    if self.special_act == "Potion":
+    if self.special_act == "Sandwich":
+      # Sandwiches restore 5 health        
       self.health += 5
-      print("{name} regained 5 health!".format(name=self.name))      
+      print("{name} eats a sandwich and regains 5 health!".format(name=self.name))      
 
     elif self.special_act == "Flying Kick":
+      # Flying kicks do 2 times base damage
       enemy.health -= self.damage*2
+      print("{name1} uses a flying kick on {name2}.".format(name1=self.name, name2=enemy.name))            
       if enemy.health <= 0:    
-        self.level += 1
+        self.damage += 1
         print("{name2} has been knocked out!".format(name2=enemy.name))
       else:
-        print("{name1} used a flying kick on {name2}. {name2} takes {damage} damage!".format(name1=self.name, name2=enemy.name, damage=int(self.damage*2)))       
+        print("{name2} takes {damage} damage!".format(name2=enemy.name, damage=int(self.damage*2)))       
 
     elif self.special_act == "Multiple Jabs":
+      # Multiple jabs do 3 times base damage 
       enemy.health -= self.damage*3
+      print("{name1} uses a multiple jabs on {name2}.".format(name1=self.name, name2=enemy.name))     
       if enemy.health <= 0 :   
-        self.level += 1
+        self.damage += 1
         print("{name2} has been knocked out!".format(name2=enemy.name))
-      else:
-        print("{name1} used multiple jabs on {name2}. {name2} takes {damage} damage!".format(name1=self.name, name2=enemy.name, damage=int(self.damage*3)))       
+      else:     
+        print("{name2} takes {damage} damage!".format(name2=enemy.name, damage=int(self.damage*3)))       
 
     elif self.special_act == "Drain":
-      begin_health = enemy.health
+      # Drain takes base damage health from the enemy
+      begin_health = enemy.health 
       enemy.health -= min(self.damage, begin_health)
       self.health += min(self.damage, begin_health)
+      print("{name1} uses drain to take {damage} health from {name2}!".format(name1=self.name, name2=enemy.name, damage=int(round(min(begin_health, self.damage),0))))         
       if enemy.health <= 0:     
-        self.level += 1
+        self.damage += 1
         print("{name2} has been knocked out!".format(name2=enemy.name))
-      else:
-        print("{name1} used drain. {name1} took {damage} health from {name2}!".format(name1=self.name, name2=enemy.name, damage=int(round(min(self.damage, begin_health),0))))         
- 
 
 
 
 def gameplay():
     # Create 2 main player characters
-    Misako = Player(name="Misako", level=5, damage=10, special_act="Drain")
-    Kyoko = Player(name="Kyoko", level=5, damage=10, special_act="Flying Kick")
+    Misako = Player(name="Misako", damage=7, special_act="Drain")
+    Kyoko = Player(name="Kyoko", damage=8, special_act="Flying Kick")
 
-    # Create enemies
+    # Create enemies with random names and random specials
     Bully = Enemy(name=random.choice(["Bully Dino", "Bully Tina", "Bully Jerome"]), 
-                        level = 2, damage = 3, special_act = "Drain")
+                        damage = 3, special_act = random.choice(["Sandwich","Flying Kick","Multiple Jabs","Drain"]))
     
     School_Girl = Enemy(name=random.choice(["School Girl Susan", "School Girl Kelly", "School Girl Hannah"]),
-                        level = 3, damage = 4, special_act = "Multiple Jabs")
+                        damage = 4, special_act = random.choice(["Sandwich","Flying Kick","Multiple Jabs","Drain"]))
     
     Punk = Enemy(name=random.choice(["Punk Danielle", "Punk Zoe", "Punk Harris"]), 
-                        level = 4, damage = 8, special_act = "Flying Kick")
+                        damage = 6, special_act = random.choice(["Sandwich","Flying Kick","Multiple Jabs","Drain"]))
     
     Tiger_Man = Enemy(name=random.choice(["Tiger Man Bob", "Tiger Man Joe", "Tiger Man Zeus"]),
-                        level = 5, damage = 10, special_act = "Potion")
+                        damage = 8, special_act = random.choice(["Sandwich","Flying Kick","Multiple Jabs","Drain"]))
     
     Yakuza = Enemy(name=random.choice(["Yakuza Tim", "Yakuza Michelle", "Yakuza Lee"]),  
-                        level = 6, damage = 11, special_act = "Multiple Jabs")
+                        damage = 9, special_act = random.choice(["Sandwich","Flying Kick","Multiple Jabs","Drain"]))
     
     # Create enemy list
     Enemy_List = [Bully, School_Girl, Punk, Tiger_Man, Yakuza]    
@@ -221,6 +235,7 @@ def gameplay():
             print("Which attack would you like to use?")
             print("Taunt, Punch, Kick, or Special Attack")
             Attack = input("Enter your attack: ")
+            print("")
              
             # Make sure the player chose a valid move 
             while Attack not in ("Taunt", "Punch", "Kick", "Special Attack"):
@@ -262,7 +277,7 @@ def gameplay():
     
     # If the player is still conscious after knocking out all enemies, they've won!
     if Active_Player.health > 0:
-        print("You won!")
+        print("You won!!!")
         print("")
         
     # Whether they've won or lost, they can play again:
@@ -279,19 +294,15 @@ def gameplay():
       gameplay()
         
       
-      
     
 gameplay()
 
 
 
-
 #To Do 
-### Add more comments 
-### Add more taunts 
-### Convert all damage to a multiple of level?
-### Additional text cleaning?
-
-### (DONE) Remove extraneous elements ("X can't attack b/c X/Y is knocked out")
-### (DONE) Make sure drain can only remove as much health as a character actually has
-
+### (DONE) Replace level with damage
+### (DONE) Add more taunts 
+### (DONE) Change potion to sandwich
+### (DONE) Give all enemies random attacks
+### (DONE) Text cleaning
+### (DONE) Add comments
